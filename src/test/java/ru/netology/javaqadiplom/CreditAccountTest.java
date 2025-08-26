b69ed0ae3e4f92b2d22a74e9d4c7c7171265e254
Anastasia <nastya.il03@mail.ru>
2025-08-26 18:47:05 +0400

    Add tests for constructors

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -3,8 +3,9 @@ package ru.netology.javaqadiplom;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class CreditAccountTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+public class CreditAccountTest {
     @Test
     public void shouldAddToPositiveBalance() {
         CreditAccount account = new CreditAccount(
@@ -15,6 +16,38 @@ public class CreditAccountTest {
 
         account.add(3_000);
 
-        Assertions.assertEquals(3_000, account.getBalance());
+        assertEquals(3_000, account.getBalance());
+    }
+
+    // выбрасывает исключение при отрицательной начальном балансе
+    @Test
+    public void shouldThrowExceptionIfInitialBalanceIsNegative() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CreditAccount(-300, 7000, 15));
+    }
+
+    // выбрасывает исключение при отрицательной максимальной сумме, которую можно задолжать банку
+    @Test
+    public void shouldThrowExceptionIfCreditLimitIsNegative() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CreditAccount(300, -7000, 15));
+    }
+
+    // выбрасывает исключение при отрицательной ставке
+    @Test
+    public void shouldThrowExceptionIfRateIsNegative() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CreditAccount(300, 7000, -15));
+    }
+
+    // нулевая ставка
+    @Test
+    public void shouldTakeZeroRate() {
+
+        CreditAccount account = new CreditAccount(300, 7000, 0);
+
+        int expected = 0;
+        int actual = account.getRate();
+
+        Assertions.assertEquals(expected, actual);
     }
 }
+
+
